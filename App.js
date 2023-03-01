31e460907c58cdde11443972290d474dd511762c
Lil Ferry <shawney134@gmail.com>
2023-03-01 18:03:05 -0500

    added script tag to the top of the page

--- App.js
+++ App.js
@@ -14,4 +14,12 @@ function App() {
   );
 }
 
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+  navbar.classList.toggle("show");
+});
+
+
 export default App;
