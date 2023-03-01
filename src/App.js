716f99bedd8172afac001f1de1ba6094f886b571
Lil Ferry <shawney134@gmail.com>
2023-03-01 17:51:51 -0500

    new js code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import './App.css'
 
-function App() {    
+function App() {
+  const [showNavbar, setShowNavbar] = useState(false);
+
   return (
     <div className="App">
+      <button className="hamburger" onClick={() => setShowNavbar(!showNavbar)}>Toggle Navbar</button>
+      <Navbar className={`navbar ${showNavbar ? 'show' : 'hide'}`} />
       {/* Your website content goes here */}
     </div>
   );
 }
-const hamburger = document.querySelector('.hamburger');
-const navbar = document.querySelector('.navbar');
-
-hamburger.addEventListener('click', function() {
-  navbar.style.height = navbar.style.height === '0px' ? '150px' : '0px';
-});
 
 export default App;
